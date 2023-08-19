39c61681ee956681176c546ca145f6d29d2704f3
poppo <kzkdrmlove@gmail.com>
2023-08-19 16:22:33 +0900

    スクロールとアニメーションを組み合わせる

--- js/script.js
+++ js/script.js
@@ -1,49 +1,65 @@
-const heading = document.querySelector('#heading');
+// const heading = document.querySelector('#heading');
 
-const titleKeyframes = {
-  // opacity: [0, 1],
-  // 上に動かす指定
-  // translate: ['0 50px', 0],
-  // 回転させる
-  // rotate: ['x 360deg', 0],
-  // 色が変化する見出し
-  // color: ['transparent', '#fff'],
-  // backgroundPosition: ['100% 0', '0 0'],
-  //ふよふよ動く見出し’横の半径/縦の半径’
-  borderRadius: [
-    '20% 50% 50% 70%/50% 50% 70% 50%',
-    '50% 20% 50% 50&/40% 40% 60% 60%',
-    '50% 40% 20% 40%/40% 50% 50% 80%',
-    '50% 50% 50% 20%/40% 40% 60% 60%',
-  ],
-};
-const titleOptions = {
-  duration: 8000,
-  // easing: 'ease',
-  direction: 'alternate',
-  iterations: Infinity,
-};
-// 何を.animate(どのように,どれくらい)
-heading.animate(titleKeyframes, titleOptions);
+// const titleKeyframes = {
+//   // opacity: [0, 1],
+//   // 上に動かす指定
+//   // translate: ['0 50px', 0],
+//   // 回転させる
+//   // rotate: ['x 360deg', 0],
+//   // 色が変化する見出し
+//   // color: ['transparent', '#fff'],
+//   // backgroundPosition: ['100% 0', '0 0'],
+//   //ふよふよ動く見出し’横の半径/縦の半径’
+//   borderRadius: [
+//     '20% 50% 50% 70%/50% 50% 70% 50%',
+//     '50% 20% 50% 50&/40% 40% 60% 60%',
+//     '50% 40% 20% 40%/40% 50% 50% 80%',
+//     '50% 50% 50% 20%/40% 40% 60% 60%',
+//   ],
+// };
+// const titleOptions = {
+//   duration: 8000,
+//   // easing: 'ease',
+//   direction: 'alternate',
+//   iterations: Infinity,
+// };
+// // 何を.animate(どのように,どれくらい)
+// heading.animate(titleKeyframes, titleOptions);
 
-const items = document.querySelectorAll('.img-item');
-// console.log(items);
+// const items = document.querySelectorAll('.img-item');
+// // console.log(items);
 
-for (let i = 0; i < items.length; i++ ) {
+// for (let i = 0; i < items.length; i++ ) {
 
-  const imageKeyframes = {
+//   const imageKeyframes = {
+//     opacity: [0, 1],
+//     filter: ['blur(20px)', 'blur(0)'],
+//     rotate: ['5deg', 0],
+//     scale: [1.1, 1],
+//     translate: ['0 80px', 0],
+//     easing: 'ease',
+//   };
+
+//   const imageOptions = {
+//     duration: 1800,
+//     delay: i * 300,
+//     fill: 'forwards',
+//   };
+//   items[i].animate(imageKeyframes, imageOptions);
+// };
+
+// 監視対象が範囲内に現れたら実行する動作
+const showKirin = (entries) => {
+  const kirinKeyframes = {
     opacity: [0, 1],
-    filter: ['blur(20px)', 'blur(0)'],
-    rotate: ['5deg', 0],
-    scale: [1.1, 1],
-    translate: ['0 80px', 0],
-    easing: 'ease',
-  };
-
-  const imageOptions = {
-    duration: 1800,
-    delay: i * 300,
-    fill: 'forwards',
-  };
-  items[i].animate(imageKeyframes, imageOptions);
+    translate: ['200px 0', 0],
+  }
+  entries[0].target.animate(kirinKeyframes, 600);
 };
+
+
+
+// 監視ロボットの設定
+const kirinObserver = new IntersectionObserver(showKirin);
+
+kirinObserver.observe(document.querySelector('#kirin'));
